7d16d14bdbd183ca5c62eb7fc2f08bb504f050ff
Scott Lobdell <slobdell@hearsaycorp.com>
2015-05-02 16:55:05 -0700

    update setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='one-rep-max',
-    version="1.0.1",
+    version="1.0.2",
     url='https://github.com/slobdell/one_rep_max',
     author="Scott Lobdell",
     author_email="scott.lobdell@gmail.com",
@@ -17,6 +17,11 @@ setup(
     include_package_data=True,
     install_requires=[],
     extras_require={},
+    entry_points = {
+        "console_scripts": [
+            "sbl-command-name = package.module:main_func_name",
+        ],
+    },
     classifiers=[
         'Programming Language :: Python :: 2.7',
         'Topic :: Other/Nonlisted Topic'],
